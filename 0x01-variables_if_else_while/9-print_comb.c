796b8d3d96ac477afbf2e78f770fec042a0b3a3c
AttahAmego <attah.amego@gmail.com>
2022-09-09 22:30:54 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - all possible combinations of single-digit
+ * numbers
+ * Description: using the main function
+ * this program prints "all possible combinations of
+ * single-digit numbers"
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
